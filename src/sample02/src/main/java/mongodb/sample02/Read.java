473b55af28ba5176593c73b78d7fae4cfe69f80e
Victor <vitongos@gmail.com>
2016-06-23 16:10:13 +0200

    Replica Set demo.

--- src/sample02/src/main/java/mongodb/sample02/Read.java
+++ src/sample02/src/main/java/mongodb/sample02/Read.java
@@ -4,26 +4,21 @@ import java.net.UnknownHostException;
 
 import org.bson.Document;
 
-import com.mongodb.*;
-import com.mongodb.client.*;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
 
-/**
- * Hello world!
- *
- */
-public class Write 
-{
-    public static void main( String[] args ) throws UnknownHostException, InterruptedException
+public class Read {
+	public static void main( String[] args ) throws UnknownHostException, InterruptedException
     {
-    	@SuppressWarnings("resource")
+        @SuppressWarnings("resource")
 		MongoClient client = new MongoClient(new ServerAddress("localhost", 30001));
         MongoCollection<Document> collection = client.getDatabase("samples").getCollection("ids");
-        collection.drop();
         
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
         	try {
-        		collection.insertOne(new Document("_id", i));
-        		System.out.println("Inserted : " + i);
+        		Document document = collection.find().sort(new Document("_id", -1)).first();
+        		System.out.println("Readed : " + document.get("_id").toString());
         	}
         	catch (Exception exception) {
         		System.out.println(exception.getMessage());
